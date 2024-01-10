21402ff718424f0f8d5e8726a27961c647f91962
AmanPathan <amanpathan526@gmail.com>
2024-01-11 02:27:39 +0530

    Update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname,'./client/build')));
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname),'./clinet/build/index.html');
+    res.sendFile(path.join(__dirname),'./client/build/index.html');
 })
 
 //Routes
