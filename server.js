9d0f56a42f0b281334b797d1db0d6a423ecc17b7
AmanPathan <amanpathan526@gmail.com>
2024-01-12 02:31:16 +0530

    Ekbar last

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const path = require('path');
 
 const PORT = process.env.PORT || 8000;
 
+const { v2: cloudinary } = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 //connecting to Database
 const connectDb = require('./database/db.js');
 connectDb();
