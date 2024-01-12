e27d20ffdd0e3f29460fb88f1ab95fa8a988e810
AmanPathan <amanpathan526@gmail.com>
2024-01-12 17:39:31 +0530

    Changed Code

--- routes/download.js
+++ routes/download.js
@@ -3,8 +3,7 @@ const File = require('../models/validate.js');
 const https = require('https');
 
 router.get('/:id',async (req,res)=>{
-    const id = req.params.id;
-    const file = await File.findOne({_id:id});
+    const file = await File.findOne({_id:req.params.id});
     if(!file) {
         return res.status(404).json({msg:"File Not Found! or Link Has Been Expired!"});
     }
