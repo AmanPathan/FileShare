bb45418b2ef29dab5a4c626e76f3f27cf4099df9
AmanPathan <amanpathan526@gmail.com>
2024-01-18 23:35:35 +0530

    Changes 2

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ const path = require('path');
 
 const PORT = process.env.PORT || 8000;
 
-const { v2: cloudinary } = require('cloudinary');
-
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 //connecting to Database
 const connectDb = require('./database/db.js');
 connectDb();
@@ -26,11 +18,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //static files for deployment
-app.use(express.static(path.join(__dirname,'./client/build')));
+// app.use(express.static(path.join(__dirname,'./client/build')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname),'./client/build/index.html');
-})
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.join(__dirname),'./client/build/index.html');
+// })
 
 //Routes
 app.use('/api/files',require('./routes/routes.js'));
